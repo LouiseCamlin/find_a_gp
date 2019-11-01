fcec1fbfeb5f01d317bf94198166459d7155aa7a
LouiseCamlin <lacamlin@gmail.com>
2019-11-01 12:05:38 +0000

    add react frontend

---
+++ client/src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import "./App.css";
+import Request from "./helpers/Request.js";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    const request = new Request();
+    const url = "http://localhost:3001/api";
+    request.get(url).then(data => {
+      this.setState(
+        {
+          gpPractices: data
+        },
+        () => console.log(this.state.gpPractices)
+      );
+    });
+  }
+
+  // getGps() {}
+
+  renderPracticeNames() {}
+
+  render() {
+    if (this.state) {
+      const practices = this.state.gpPractices.map((practice, index) => {
+        return <li key={index}>{practice.GPPracticeName}</li>;
+      });
+      return <ul>{practices}</ul>;
+    }
+    return <h1>loading...</h1>;
+  }
+}
+
+export default App;
