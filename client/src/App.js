866553271f64caa551cd077baeb41d6d486a1c45
LouiseCamlin <lacamlin@gmail.com>
2019-12-18 16:39:21 +0000

    implement router in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,64 +1,26 @@
 import React, { Component } from "react";
-import "./App.css";
-import Request from "./helpers/Request.js";
-import GPList from "./components/GPList.js";
-import SearchBar from "./components/SearchBar";
+import MainContainer from "./containers/MainContainer";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import GPItem from "./components/GPItem";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      searchResults: [],
-      gpPractices: []
-    };
-    this.onHandleClick = this.onHandleClick.bind(this);
-  }
-  componentDidMount() {
-    const request = new Request();
-    const url = "http://localhost:3001/api";
-    request.get(url).then(data => {
-      const newData = data.map(practice => {
-        practice.fullAddress = [
-          practice.AddressLine1.toLowerCase(),
-          practice.AddressLine2.toLowerCase(),
-          practice.AddressLine3.toLowerCase(),
-          practice.AddressLine4.toLowerCase(),
-          practice.Postcode.toLowerCase()
-        ];
-        return practice;
-      });
-
-      this.setState({ gpPractices: newData });
-    });
-  }
-
-  filterAddresses(searchString) {
-    const filteredAddresses = this.state.gpPractices.filter(practice =>
-      practice.fullAddress.toString().includes(searchString)
-    );
-    console.log("filteredAddresses", filteredAddresses);
-    this.setState({
-      searchResults: filteredAddresses
-    });
-  }
-
-  onHandleClick(searchString) {
-    this.filterAddresses(searchString);
-  }
-
   render() {
-    if (this.state.gpPractices) {
-      return (
-        <>
-          <SearchBar
-            practices={this.state.gpPractices}
-            onHandleClick={this.onHandleClick}
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={MainContainer} />
+          <Route
+            exact
+            path="/:id"
+            render={props => {
+              const id = props.match.params.id;
+              return <GPItem id={id} />;
+            }}
           />
-          <GPList filteredPractices={this.state.searchResults} />
-        </>
-      );
-    }
-    return <h1>loading...</h1>;
+        </Switch>
+      </Router>
+    );
+    // return <MainContainer />;
   }
 }
 
